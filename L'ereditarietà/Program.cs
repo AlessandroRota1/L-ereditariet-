67759a93b0600a3a7b8a69cc2f7cc571b7cb8a02
Rota.20073 <rotaale05102006@gmail.com>
2023-11-28 11:04:52 +0100

    Implementazione area e perimetro del quadrato e volume del cubo

--- L'ereditarietà/Program.cs
+++ L'ereditarietà/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace L_ereditarietà
 {
+    class quadrato
+    {
+        protected double _lato;
+        public quadrato(double lato)
+        { 
+            this._lato = lato; 
+        }
+        public void perimetro()
+        {
+            Console.WriteLine("Il perimetro è " + _lato * 4);
+        }
+        public void area()
+        {
+            Console.WriteLine("L'area è " + _lato * _lato);
+        }
+    }
+
+    class cubo:quadrato
+    {
+        public cubo(double lato) : base(lato) 
+        {
+            _lato = lato;
+        }
+        public void superificie() 
+        { 
+            Console.WriteLine("La superificie è " + _lato * _lato * 6);
+        }
+        public void volume()
+        {
+            Console.WriteLine("IL volume è " + _lato * _lato * _lato);
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci il lato");
+            double latoo= double.Parse(Console.ReadLine());
+            quadrato Quadrato = new quadrato(latoo);
+            cubo Cubo = new cubo(latoo);
+            Quadrato.perimetro();
+            Quadrato.area();
+            Cubo.superificie();
+            Cubo.volume();
         }
     }
 }
